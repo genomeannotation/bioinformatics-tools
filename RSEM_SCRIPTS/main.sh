0bda49b0766706b0b4601824af705c57f5e36c9a
Brian Hall <bhall7@hawaii.edu>
2013-06-25 13:55:09 -1000

    refactored RSEM main script

--- RSEM_SCRIPTS/main.sh
+++ RSEM_SCRIPTS/main.sh
@@ -3,94 +3,48 @@
 # checking for errors as it goes...
 
 source SCRIPTS/options.cfg
+PATH_TO_RSEM_SCRIPTS="SCRIPTS/RSEM_SCRIPTS"
 
-if [ ! -d RawReads/concatenated ]
-then
-	echo "About to call concatenate_raw_reads.sh"
-	SCRIPTS/RSEM_SCRIPTS/concatenate_raw_reads.sh
-	if [ "$?" -ne 0 ]
+function CheckDirectoryAndRunScript {
+# If directory $1 doesn't exist, run script $2
+	if [ ! -d $1 ]
 	then
-		echo "concatenate_raw_reads.sh failed, exiting now."
-		exit 1
-	fi 
-	echo "concatenate_raw_reads.sh complete."
-	echo ""
-fi
-
-if [ ! -d FilterAssembly/RSEM_output ]
-then
-	echo "About to call estimate_abundance.sh"
-	SCRIPTS/RSEM_SCRIPTS/estimate_abundance.sh
-	if [ "$?" -ne 0 ]
-	then
-		echo "estimate_abundance.sh failed, exiting now."
-		exit 1
+		echo "about to call $2"
+		$PATH_TO_RSEM_SCRIPTS/$2
+		if [ "$?" -ne 0 ]
+		then
+			echo "$2 failed; exiting now."
+			exit -1
+		fi
+	echo "$2 complete"
 	fi
-	echo "estimate_abundance.sh complete."
-	echo ""
-fi
+}
 
-if [ ! -f FilterAssembly/RSEM_output/Trinity.RSEM_filtered.fasta ]
-then
-	echo "About to call filter_by_RSEM.sh"
-	SCRIPTS/RSEM_SCRIPTS/filter_by_RSEM.sh
-	if [ "$?" -ne 0 ]
+function CheckFileAndRunScript {
+# If file $1 doesn't exist, run script $2
+	if [ ! -s $1 ]
 	then
-		echo "filter_by_RSEM.sh failed, exiting now."
-		exit 1
+		echo "about to call $2"
+		$PATH_TO_RSEM_SCRIPTS/$2
+		if [ "$?" -ne 0 ]
+		then
+			echo "$2 failed; exiting now."
+			exit -1
+		fi
+	echo "$2 complete"
 	fi
-	echo "filter_by_RSEM.sh complete."
-	echo ""
-fi
+}
 
-if [ ! -d FilterAssembly/TransdecoderOutput ]
-then
-	echo "About to call extract_best_orfs.sh"
-	SCRIPTS/RSEM_SCRIPTS/extract_best_orfs.sh
-	if [ "$?" -ne 0 ]
-	then
-		echo "extract_best_orfs.sh failed, exiting now."
-		exit 1
-	fi
-	echo "extract_best_orfs.sh complete."
-	echo ""
-fi
+CheckDirectoryAndRunScript "RawReads/concatenated" "concatenate_raw_reads.sh"
 
-if [ ! -f $RSEM_CODING_FASTA ]
-then
-	echo "About to call filter_by_coding.sh"
-	SCRIPTS/RSEM_SCRIPTS/filter_by_coding.sh
-	if [ "$?" -ne 0 ]
-	then
-		echo "filter_by_coding.sh failed, exiting now."
-		exit 1
-	fi
-	echo "filter_by_coding.sh complete."
-	echo ""
-fi
+CheckDirectoryAndRunScript "FilterAssembly/RSEM_output" "estimate_abundance.sh"
 
-if [ ! -d IndividualLibraryMappings ]
-then
-	echo "About to call rsem_each_library.sh"
-	SCRIPTS/RSEM_SCRIPTS/rsem_each_library.sh
-	if [ "$?" -ne 0 ]
-	then
-		echo "rsem_each_library.sh failed, exiting now."
-		exit 1
-	fi
-	echo "rsem_each_library.sh complete."
-	echo ""
-fi
+CheckFileAndRunScript "FilterAssembly/RSEM_output/Trinity.RSEM_filtered.fasta" "filter_by_RSEM.sh"
 
-if [ ! -f IndividualLibraryMappings/library_alignment_info ]
-then
-	echo "About to call get_individual_alignment_data.sh"
-	SCRIPTS/RSEM_SCRIPTS/get_individual_alignment_data.sh
-	if [ "$?" -ne 0 ]
-	then
-		echo "get_individual_alignment_data.sh failed, exiting now"
-		exit 1
-	fi
-	echo "get_individual_alignment_data.sh complete."
-	echo ""
-fi
+CheckDirectoryAndRunScript "FilterAssembly/TransdecoderOutput" "extract_best_orfs.sh"
+
+CheckFileAndRunScript "$RSEM_CODING_FASTA" "filter_by_coding.sh"
+
+CheckDirectoryAndRunScript "IndividualLibraryMappings" "rsem_each_library.sh"
+
+CheckFileAndRunScript "IndividualLibraryMappings/library_alignment_info" "get_individual_alignment_data.sh"
