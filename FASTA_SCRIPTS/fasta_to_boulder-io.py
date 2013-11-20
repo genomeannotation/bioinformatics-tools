d0760ed5134cc48d008b387ca8a8328b98322566
Brian Hall <bhall7@hawaii.edu>
2013-11-19 14:28:01 -1000

    fasta to boulder converter added

---
+++ FASTA_SCRIPTS/fasta_to_boulder-io.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import Bio
+from Bio import SeqIO
+
+if len(sys.argv) != 2:
+    print "usage: python fasta_to_boulder-io.py <input.fasta>"
+    sys.exit()
+
+input_fasta = sys.argv[1]
+
+for seq_record in SeqIO.parse(input_fasta, "fasta"):
+    print("SEQUENCE_ID="+seq_record.id)
+    print("SEQUENCE_TEMPLATE="+seq_record.seq)
+
