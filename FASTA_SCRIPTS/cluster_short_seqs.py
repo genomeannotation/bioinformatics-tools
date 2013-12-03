df066eadca82ece24bbd62c9c52c0a9df32caece
Brian Hall <bhall7@hawaii.edu>
2013-12-03 12:06:31 -1000

    cluster_short_seqs gets a friend

--- FASTA_SCRIPTS/cluster_short_seqs.py
+++ FASTA_SCRIPTS/cluster_short_seqs.py
@@ -1,39 +1,80 @@
 #!/usr/bin/env python
 
+# TODO make exceptions_allowed a %, and a cmdlinearg
+# TODO make histogram
+
+
 import sys
+from trim_cluster import *
 
-input_file = sys.argv[1]
-min_length = int(sys.argv[2])
-current_cluster = ""
+input_file = ""
+min_length = 0
+min_cluster_size = 0
+current_cluster_prefix = ""
+current_cluster = []
 number_of_matches = 0
+unclustered = []
+exceptions_allowed = 4
 
-def matches_current_cluster(seq):
-    global current_cluster, min_length
-    print("matches_current_cluster here, comparing " + current_cluster + " to " + seq[:min_length])
-    return seq[:min_length] == current_cluster
+def verify_inputs():
+    if len(sys.argv) != 4:
+        sys.stderr.write("usage: python cluster_short_seqs.py <input_file> <length_to_cluster_on> <min_cluster_size>\n")
+        sys.exit()
+    else:
+        global input_file, min_length, min_cluster_size
+        input_file = sys.argv[1]
+        min_length = int(sys.argv[2])
+        min_cluster_size = int(sys.argv[3])
+        
+
+def matches_current_cluster_prefix(seq):
+    global current_cluster_prefix, min_length
+    #print("matches_current_cluster_prefix here, comparing " + current_cluster_prefix + " to " + seq[:min_length])
+    return seq[:min_length] == current_cluster_prefix
 
 def wrap_up_cluster(line):
-    global current_cluster, number_of_matches, min_length
-    print("wrapping up cluster " + current_cluster)
-    print("number_of_matches=" + str(number_of_matches))
-    print("done wrapping up\n")
-    current_cluster = line[:min_length]
+    global current_cluster, current_cluster_prefix, number_of_matches, min_length
+    #print("wrapping up cluster " + current_cluster_prefix)
+    #print("number_of_matches=" + str(number_of_matches))
+    if number_of_matches < min_cluster_size:
+        unclustered.extend(current_cluster)
+    else:
+        sys.stdout.write("cluster " + current_cluster_prefix)
+        sys.stdout.write(" (" + str(number_of_matches) + " seqs):\n")
+        sys.stdout.write("longest match with "+ str(exceptions_allowed))
+        sys.stdout.write(" exceptions allowed:\n        ")
+        sys.stdout.write(trim_cluster(current_cluster, exceptions_allowed))
+        sys.stdout.write("\n\n")
+    current_cluster = []
+    current_cluster_prefix = line[:min_length]
+    #print("current prefix is " + current_cluster_prefix)
     number_of_matches = 0
+    #print("done wrapping up\n")
 
 def process_line(line):
     global number_of_matches
-    print("processing line " + line)
+    #print("processing line " + line)
+    current_cluster.append(line.strip())
     number_of_matches += 1
 
 def new_cluster(line):
-    return not matches_current_cluster(line[:min_length])
+    return not matches_current_cluster_prefix(line[:min_length])
+
+##################
+# BEGIN PROGRAM :)
+##################
+
+verify_inputs()
 
 with open(input_file, 'r') as file:
     for line in file:
         if new_cluster(line):
-            print("new_cluster returned true on " + line)
+    #        print("new_cluster returned true on " + line)
             wrap_up_cluster(line)
         process_line(line)
     # take care of last cluster
     wrap_up_cluster(line)
+
+sys.stdout.write("unclustered: ")
+sys.stdout.write("(" + str(len(unclustered)) + " seqs): " + str(unclustered))
 
---
+++ FASTA_SCRIPTS/trim_cluster.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+def length_of_longest_read(cluster):
+    max = 0
+    for seq in cluster:
+        if len(seq) > max:
+            max = len(seq)
+    return max
+
+def enough_seqs_match(cluster, match_length, exceptions_allowed):
+    mismatches = 0
+    # NOTE: THIS BREAKS IF THE FIRST SEQUENCE IS AN EXCEPTION :(
+    target_sequence = cluster[0][:match_length]
+    for seq in cluster:
+        if seq[:match_length] != target_sequence[:match_length]:
+            mismatches += 1
+    if mismatches > exceptions_allowed:
+        return False
+    else:
+        return True
+    
+
+def trim_cluster(cluster, exceptions_allowed):
+    max_length = length_of_longest_read(cluster)
+    number_of_seqs = len(cluster)
+    for i in xrange(1, max_length+1):
+        if not enough_seqs_match(cluster, i, exceptions_allowed):
+            return cluster[0][:i-1]
+    return cluster[0]
+
